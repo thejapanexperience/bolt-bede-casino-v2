b78b6fe390f169b01b41e8260f71f966fba36b85
Michael K. Clements <michaelkc@me.com>
2018-11-09 16:43:29 +0000

    Starting to add contentful templates to the build

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,5 +3,43 @@
  *
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
+const path = require('path')
 
-// You can delete this file if you're not using it
+exports.createPages = ({ actions, graphql }) => {
+  const { createPage } = actions
+
+  const contentTemplate = path.resolve(`src/templates/contentTemplate.js`)
+
+  return new Promise((resolve, reject) => {
+    resolve(
+      graphql(
+        `
+          {
+            allContentfulContent(filter: {node_locale: {regex: "/en-GB/"}}) {
+              edges {
+                node {
+                  slug
+                }
+              }
+            }
+          }
+        `
+      ).then(result => {
+        if (result.errors) {
+          console.log(result.errors)
+          reject(result.errors)
+        }
+
+        result.data.allContentfulContent.edges.forEach(({ node }) => {
+          createPage({
+            path: node.slug,
+            component: contentTemplate,
+            context: {
+              slug: node.slug
+            }, // additional data can be passed via context
+          })
+        })
+      })
+    )
+  })
+}
